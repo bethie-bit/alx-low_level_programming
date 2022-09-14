f5d668beb80def731b4a0a54c064788bffeb5458
bethie-bit <elizabethfadenipo@gmail.com>
2022-09-14 16:35:54 -0700

    mm

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - void print_alphabet_x10(void);
+ *
+ *Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch + 'a'; ch <= 'z''; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
