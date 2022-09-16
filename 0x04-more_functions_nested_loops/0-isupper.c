2db9cd27d04b628b8c3308226da64f21160a8f4c
bethie-bit <elizabethfadenipo@gmail.com>
2022-09-16 13:26:22 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - check if c is upper
+ * _isupper - checkes if the input is upper
  * @c: input alphabet
  *
  * Return: 1(upper), 0(otherwise)
@@ -9,8 +9,8 @@
 int _isupper(int c)
 {
 	int i;
-	
-	if ((c <= "A") && (c >= "Z"))
+
+	if ((c >= 'A') && (c <= 'Z'))
 		i = 1;
 	else
 		i = 0;
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,3 @@
-alx
+# More Functions and Nested Loops
+
+This project contains even more challenging tasks on functions and loops
