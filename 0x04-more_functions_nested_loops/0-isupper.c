16268f5c5964b8bc1b491c36075e4697202e4e3a
bethie-bit <elizabethfadenipo@gmail.com>
2022-09-16 07:13:40 -0700

    hmmm

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main int _isupper(int c);
+ *
+ * Return 1(uppercase), 0(other)
+ */
+int _isupper(int c);
+{
+	int i;
+
+	if ((c >= 'A') && (c <= 'Z'))
+		i = 1
+	else
+		i = 0;
+	return (i);
+}
